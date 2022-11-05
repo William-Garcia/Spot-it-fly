ee564a78d7bea1773871fc70a3412073ee0755a8
William Garcia-Sanchez <89278772+Wixx19@users.noreply.github.com>
2022-11-05 16:25:18 -0400

    first

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: 'c08430b0753e4d7bb5918342805ec129',
+        ClientSecret: '7936edeb9e3e41e69c91f07d24bc4d2d'
+    }
+}
+
+export { Credentials };
